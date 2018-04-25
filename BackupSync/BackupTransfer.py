e56ca47a97fc8c7e8863db5f8f66c3a9aa7b6def
Robert Hartshorn <roberthartshorn@midstatespring.com>
2018-04-25 17:35:53 -0400

    Implement a main method

--- BackupSync/BackupTransfer.py
+++ BackupSync/BackupTransfer.py
@@ -2,29 +2,32 @@ from shutil import copytree
 import os, subprocess, datetime, time, signal
 from afp_utils import afp_mount
 
-src='/Users/roberthartshorn/Code/Python Projects/BackupSync/Server Backup Staging/Current'
-dest='/Users/roberthartshorn/Code/Python Projects/BackupSync/Server Backups/Current'
+def main():
+	src='/Users/roberthartshorn/Code/Python Projects/BackupSync/Server Backup Staging/Current'
+	dest='/Users/roberthartshorn/Code/Python Projects/BackupSync/Server Backups/Current'
 
-src_url='"afp://sadmin:BRLa4rxx@fugue/S_B_RAID"'
-dest_url='"afp://admin:BRLa4rxx@nas1/NetworkBackups"'
+	src_afp_url='"afp://sadmin:BRLa4rxx@fugue/S_B_RAID"'
+	dest_afp_url='"afp://admin:BRLa4rxx@nas1/NetworkBackups"'
 
-'''TODO: Add a method to verify src/ and dest/ exist.  *Don't forget os.path.isdir' '''
+	'''This dictionary maps a host to a list of expected backup volumes.'''
+	'''TODO:  Consider converting this to use json'''
+	expected_backups = {
+		'beyonce' :['ServerHD.sparseimage','AppleCore1.sparseimage'],
+		'epicor' : ['C','D']
+	}
 
-'''TODO:  Add a method to verify that there is a top-level folder in src/ and dest/ for each host in 'expected_backups' '''
+	print afp_mount(src_afp_url,dest_afp_url)
+	try:
+		'''TODO: consider whether or not we want to use shutil.copystat'''
+		copytree(src,dest)
+	except Exception as e:
+		print e
 
-'''TODO:  Convert this to use json'''
+if __name__ == '__main__':
+	print main()
 
-'''This dictionary maps a host to its volumes.  Only includes volumes that are imaged.'''
-expected_backups = {
-	'beyonce' :['ServerHD.sparseimage','AppleCore1.sparseimage'],
-	'epicor' : ['C','D']
-}
 
-print afp_mount(src_url,dest_url)
-try:
-	copytree(src,dest)
-except:
-	print "could not copy"
+
   
-'''TODO: consider whether or not we want to use shutil.copystat
+
           
