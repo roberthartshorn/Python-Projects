c15f276bb29ee294b7a969cbce543cc561895dc2
Robert Hartshorn <roberthartshorn@midstatespring.com>
2021-02-16 08:31:33 -0500

    General Updates

--- InspectionMigrator/main.py
+++ InspectionMigrator/main.py
@@ -1,25 +1,44 @@
 import requests
 import requests_ntlm
+import os
 from lxml import html
 import json
+import time
 
 #URL = "http://velocity/"
-URL = "http://velocity/review.asp?action=review&WorkOrder=35999&PartID=&QC=&PONum=&POLine="
+jobURL = "http://velocity/review.asp?action=review&WorkOrder="
+inspURL = "http://velocity/review.asp?action=review&QC="
+dest = "/Users/roberthartshorn/Desktop/Inspection Exports/"
+#jobsFile = dest + "jobslist.txt"
+jobsFile = "/Users/roberthartshorn/Desktop/jobslist.txt"
 
 def main():
-    response = requests.get(URL, auth=requests_ntlm.HttpNtlmAuth('roberthartshorn', 'LaylaClapton3'))
+  os.chdir(dest)
+
+  with open(jobsFile, "r") as f:
+    jobs = f.read()  
+
+  for job in jobs.split(","): 
+    resultSet = []
+    print(job)
+    response = requests.get(jobURL + job, auth=requests_ntlm.HttpNtlmAuth('inspectionmigrator', 'inspectionmigrator'))
+    print(job)
     print(response.status_code)
-   # print(response.content)
     
-    doc = html.fromstring(response.content)
-    # resultSet = doc.xpath('//head/title/table[@class="top"')
-    resultSet = doc.xpath("//body//table[@class='toptab']//table//tr")
-    print(resultSet)
-  #/tr/tdtd/table
-  
-    #html.open_in_browser(doc) 
-    #responseJSON = json.dumps(response.text)
-    #print(responseJSON)
+    try:
+      doc = html.fromstring(response.content)
+      resultSet = doc.xpath("//body//table[@class='toptab']//table//tr//td/a")
+    except:
+      with open("log.txt", "a") as f:
+        f.write(job + "\n")
+
+    for count, x in enumerate(resultSet):
+      resp = requests.get(inspURL + x.text, auth=requests_ntlm.HttpNtlmAuth('inspectionmigrator', 'inspectionmigrator'))
+      filename = job + "." + str(count) + ".html"
+      with open(filename, "wb") as f:
+        f.write(resp.content)
+
+    time.sleep(3)
 
 if __name__ == "__main__":
     main()
